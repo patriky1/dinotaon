cc63d9ecb4018418f8a252a891914b8d0c200964
patriky brito <patrikybrito@gmail.com>
2023-09-30 19:09:33 -0300

    addpages

--- src/App.js
+++ src/App.js
@@ -1,36 +1,16 @@
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-import { Button } from 'react-bootstrap';
-import NavbarContainer from "./components/Navbar"
-// importação de imagens
-import fundo from "./imagens/out.jpeg"
-// importação do carrosel
-import Slider from './slider';
-
-//importação pagina 2
-
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import NavbarContainer from "./components/Navbar";
+import Slider from "./slider";
+import Home from "./components/Home";
 
 function App() {
   return (
-<>
-
-        <NavbarContainer/>
-        <div className='grid'>
-          <img  className='background' src={fundo} alt='...' ></img>
-          <h1 className='textogrid'>Sousa - Cidade Sorriso</h1>
-          <h2 className='textogrid1'>Bem-vindo à encantadora cidade de Sousa, 
-          um tesouro no coração da Paraíba! Sousa, 
-          carinhosamente conhecida como "Terra dos Dinossauros", 
-          é um destino único que combina história, cultura, 
-          esportes e um rico patrimônio paleontológico.</h2>
-          <Button href="#eventos" variant="warning" size="lg" className='buttonone'> saiba mais</Button>
-        </div>
-      <div className='session2' id='eventos'>
-        <Slider />
-      </div>
-
-</>
+    <>
+      <NavbarContainer />
+      <Home/>
+      <Slider/>
+    </>
   );
 }
 
---
+++ src/components/Home.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Button } from "react-bootstrap";
+import fundo from "../imagens/out.jpeg";
+
+
+function Home() {
+  return (
+    <>
+      <div className="grid">
+        <img className="background" src={fundo} alt="..."></img>
+        <h1 className="textogrid">Sousa - Cidade Sorriso</h1>
+        <h2 className="textogrid1">
+          Bem-vindo à encantadora cidade de Sousa, um tesouro no coração da
+          Paraíba! Sousa, carinhosamente conhecida como "Terra dos Dinossauros",
+          é um destino único que combina história, cultura, esportes e um rico
+          patrimônio paleontológico.
+        </h2>
+        <Button
+          href="#eventos"
+          variant="primary"
+          size="lg"
+          className="buttonone"
+        >
+          {" "}
+          saiba mais
+        </Button>
+      </div>
+    </>
+  );
+}
+
+export default Home;
