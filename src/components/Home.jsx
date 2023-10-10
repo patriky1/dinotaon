8a7729e1f0c80f5c826020825b464482ee984433
patriky brito <patrikybrito@gmail.com>
2023-10-10 19:05:53 -0300

    estilização

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,27 +5,33 @@ import fundo from "../imagens/fundo.jpeg";
 import "./Home.css";
 import { Link } from "react-router-dom";
 
-
 function Home() {
   return (
     <>
       <Container fluid>
         <Row>
-          <Col lg={5} xs={13} className="grid">
-            <h1 className="textogrid">SOUSA - CIDADE SORRISO DA PARAÍBA</h1>
-            <p className="legend">
+        <Col lg={12} xs={12} className="Lateral">
+            <img
+              style={{ margin:"0" }}
+              className="background"
+              src={fundo}
+              alt="..."
+            ></img>
+          </Col>
+          <Col lg={12} xs={13} className="grid">
+            {/* <h1 className="textogrid">SOUSA - CIDADE SORRISO DA PARAÍBA</h1> */}
+            <p className="legend" style={{ marginTop:"5%"}}>
               {" "}
-              No dia 04 de junho de 1800 o Ouvidor Geral José da Silva Coutinho
-              instala, oficialmente, a Vila Nova de Sousa através de Resolução
-              do então Governador de Pernambuco, datada de 26 de março de 1800,
-              após pleito da comunidade através de requerimento encabeçado por
-              Patrício José de Almeida, Matias de Figueiredo Rocha e padre
-              Manoel Vieira da Silva. Um dia antes, o Capitão Alexandre Pereira
-              de Sousa fez uma doação de terras para o patrimônio do crescente
-              povoado. Foi através da Lei Provincial de n° 28, de 10 de julho de
-              1854, que a Vila de Sousa foi elevada à categoria de cidade
-              passando, na oportunidade, a denominar-se Sousa, conhecida hoje
-              por “cidade sorriso”.
+              <span style={{fontSize:"larger"}} className="highlight">Sousa</span>, carinhosamente conhecida como a "Cidade Sorriso", é um
+              verdadeiro tesouro no coração do estado da Paraíba, no nordeste do
+              Brasil. Localizada no Sertão paraibano, Sousa combina a beleza
+              natural do semiárido com uma atmosfera vibrante e acolhedora. A
+              cidade também é famosa por abrigar um verdadeiro tesouro
+              pré-histórico: o Vale dos Dinossauros. Com sua rica herança
+              paleontológica, o município é um destino imperdível para os
+              amantes da história da Terra. Bem-vindo a Sousa, onde o sorriso de
+              suas pessoas e os segredos dos dinossauros se encontram em um
+              cenário verdadeiramente especial.
             </p>
             <Link
               style={{ textDecoration: "none" }}
@@ -37,32 +43,23 @@ function Home() {
               </Button>
             </Link>
           </Col>
-          <Col lg={7} xs={12} className="Lateral">
-            <img className="background" src={fundo} alt="..."></img>
-          </Col>
+          
         </Row>
       </Container>
-      <Container
-        
-        style={{ background: "#FFFF" }}
-        fluid
-      >
-        <Row >
+      <Container style={{ background: "#FFFF" }} fluid>
+        <Row>
           <Col className="d-flex justify-content-center">
             <Link to="/vale" style={{ textDecoration: "none" }}>
               <Card
                 className="carddino"
                 style={{
-                  display:"flex",
-                  justifyContent:"center",
+                  display: "flex",
+                  justifyContent: "center",
                   border: "none",
                   background: "transparent",
                 }}
               >
-                <Card.Img
-                  
-                  src="	https://cdn-icons-png.flaticon.com/128/7416/7416943.png"
-                />
+                <Card.Img src="	https://cdn-icons-png.flaticon.com/128/7416/7416943.png" />
                 <Card.Title className="cardTitle">TURISMO</Card.Title>
               </Card>
             </Link>
@@ -70,32 +67,24 @@ function Home() {
           <Col className="d-flex justify-content-center">
             <Link
               to="/igrejas"
-              style={{ color: "black",
-              textDecoration: "none",
-              }}>
-              <Card
-              style={{border: "none",
-              background: "transparent"}}>
-              <Card.Img
-              src="https://cdn-icons-png.flaticon.com/128/2242/2242235.png"/>
-              <Card.Title className="cardTitle">CULTURA</Card.Title>
+              style={{ color: "black", textDecoration: "none" }}
+            >
+              <Card style={{ border: "none", background: "transparent" }}>
+                <Card.Img src="	https://img.icons8.com/?size=80&id=0aLX4JgODHJw&format=png" />
+                <Card.Title className="cardTitle">CULTURA</Card.Title>
               </Card>
             </Link>
           </Col>
           <Col className="d-flex justify-content-center">
             <Link
               to="/centroc"
-              style={{ color: "black", textDecoration: "none" }}>
-              <Card
-              style={{border: "none",
-              background: "transparent"}}>
-              <Card.Img
-              src="	https://cdn-icons-png.flaticon.com/128/3019/3019220.png"
-              />
-              <Card.Title className="cardTitle">EVENTOS</Card.Title>
+              style={{ color: "black", textDecoration: "none" }}
+            >
+              <Card style={{ border: "none", background: "transparent" }}>
+                <Card.Img src="	https://cdn-icons-png.flaticon.com/128/3019/3019220.png" />
+                <Card.Title className="cardTitle">EVENTOS</Card.Title>
               </Card>
             </Link>
-            
           </Col>
         </Row>
       </Container>
